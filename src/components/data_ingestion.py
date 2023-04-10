d608fa374b968b5600458a5330feff87ab2a9c05
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-04-11 03:12:43 +0500

    data ingestion - dataclass

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+import os
+import sys
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
 
 @dataclass
 class DataIngestionConfig():
-    train_data_path = 
+    train_data_path = os.path.join('')
