3f07006e60bf150e76a6bc981e3498fae27a3b97
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-04-11 03:17:56 +0500

    config defined

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,5 +8,7 @@ from sklearn.model_selection import train_test_split
 
 
 @dataclass
-class DataIngestionConfig():
-    train_data_path = os.path.join('')
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw.csv')
