449dd07185c3b1f0e31f025a06d2ec838ef3dd34
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-04-11 19:54:34 +0500

    evaluate models defined

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 
 def save_object(obj, file_path):
@@ -13,5 +14,25 @@ def save_object(obj, file_path):
 
         with open(file_path, "wb") as f:
             dill.dump(obj, f)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(self, X_train, y_train, X_test, y_test, models):
+    try:
+        evaluation_report = {}
+
+        for key in models.keys():
+            model = models[key]
+            model.fit(X_train)
+
+            y_train_pred = model.predict(X_train)
+            y_train_score = r2_score(y_train, y_train_pred)
+
+            y_test_pred = model.predict(X_test)
+            y_test_score = r2_score(y_test, y_test_pred)
+
+            evaluation_report[key] = y_test_score 
+            
     except Exception as e:
         raise CustomException(e, sys)
