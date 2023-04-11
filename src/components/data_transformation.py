ff391926fe8160c3193d2ee17c9110413c1b18da
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-04-11 17:13:00 +0500

    done with data trans config

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,11 +1,38 @@
 import sys
 from src.exception import CustomException
-from src.logger import logging 
+from src.logger import logging
 from dataclasses import dataclass
+import os
 
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline 
+from sklearn.pipeline import Pipeline
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+
+
+class DataTransformation:
+
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_preprocessor():
+
+        num_features = [
+            'reading score',
+            'writing score'
+        ]
+
+        cat_features = [
+            'gender',
+            'parental level of education',
+            'lunch',
+            'race/ethnicity',
+            'test preparation course'
+        ]
