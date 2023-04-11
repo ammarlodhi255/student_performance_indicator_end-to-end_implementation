cfee3f219cfd058f01cac4c26f8ebb1f07cc6a8d
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-04-11 17:22:18 +0500

    created the function get_preprocessor

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -22,17 +22,58 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
 
-    def get_preprocessor():
+    def get_preprocessor(self):
+        '''
+        This function creates: 
+        1. Two lists (numeric and categorical features).
+        2. Two pipelines (numeric and categorical)
 
-        num_features = [
-            'reading score',
-            'writing score'
-        ]
+        returns: preprocessor of type ColumnTransformer that contains the two pipelines
+        for preprocessing the columns of each type. 
+        '''
 
-        cat_features = [
-            'gender',
-            'parental level of education',
-            'lunch',
-            'race/ethnicity',
-            'test preparation course'
-        ]
+        try:
+            num_features = [
+                'reading score',
+                'writing score'
+            ]
+
+            cat_features = [
+                'gender',
+                'parental level of education',
+                'lunch',
+                'race/ethnicity',
+                'test preparation course'
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    # since there are outliers in the data, we use strategy=median.
+                    ("Imputer", SimpleImputer(strategy=median)),
+                    ("StandardScalar", StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("Imputer", SimpleImputer(strategy=most_frequent)),
+                    ("OneHotEncoder", OneHotEncoder()),
+                    ("StandardScalar", StandardScaler())
+                ]
+            )
+
+            logging.info('Numeric and categorical pipelines created')
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("Numerical Transformation", num_pipeline, num_features),
+                    ("Categorical Transformation", cat_pipeline, cat_features)
+                ]
+            )
+
+            logging.info('Preprocessor created successfully')
+            
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
